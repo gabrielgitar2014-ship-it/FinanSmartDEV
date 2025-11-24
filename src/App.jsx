12ea02819a39c0fe1f0bc0bbb2915340a858f1a6
gabrielgitar2014-ship-it <gabriel.gitar2014@gmail.com>
2025-11-24 11:00:54 -0300

    Change import style for PrivateRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import AppLayout from "./layout/AppLayout";
-import PrivateRoute from "./components/PrivateRoute";
+import {PrivateRoute} from "./components/PrivateRoute";
 
 import LoginPage from "./pages/LoginPage";
 import OnboardingPage from "./pages/OnboardingPage";
