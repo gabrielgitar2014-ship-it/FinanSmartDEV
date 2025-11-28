b8730781d39bf349c7d2672e050fedb80ff77a8e
gabrielgitar2014-ship-it <gabriel.gitar2014@gmail.com>
2025-11-27 22:51:33 -0300

    Delete src directory

--- src/store/useSyncStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-export const useSyncStore = create((set) => ({
-  // Cada vez que este número mudar, a aplicação inteira sabe que precisa se atualizar
-  syncVersion: 0,
-  
-  // Função que o Realtime vai chamar para "avisar" que houve mudança
-  triggerSync: () => set((state) => ({ syncVersion: state.syncVersion + 1 }))
-}))
